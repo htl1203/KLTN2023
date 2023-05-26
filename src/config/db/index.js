c3c4fc02347335cfbea82b416495b3534a61a014
Lan <tlinh6005@gmail.com>
2023-05-26 12:29:06 +0700

    themgiohang

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/lenpharmacy', {
+    await mongoose.connect('mongodb://127.0.0.1:27017/lenpharmacy', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
